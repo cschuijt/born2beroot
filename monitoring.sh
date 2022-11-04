b564e6fd8a1eeeff660a1c5031e1460d9d3c9eb0
Casper Schuijt <cschuijt@student.codam.nl>
2022-11-04 14:51:08 +0100

    Initial commit

---
+++ monitoring.sh
@@ -0,0 +1,24 @@
+echo -n "#Architecture: "
+uname -a
+echo -n "#Physical CPUs: "
+# funny thing
+echo -n "#VCPU"
+# command
+echo -n "#Memory Usage: "
+# command
+echo -n "#Disk Usage: "
+# command
+echo -n "#CPU Load: "
+# command
+echo -n "#Last boot: "
+# command
+echo -n "#LVM Use: "
+# command
+echo -n "#TCP Connections: "
+# command
+echo -n "#Users logged in: "
+# command
+echo -n "#Network IP: "
+# command
+echo -n "#Sudo commands: "
+# command
