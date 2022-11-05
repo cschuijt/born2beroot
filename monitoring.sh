592d488f08d21271c8d773d94cbdcdf11ccd995f
Casper Schuijt <casperschuijt@gmail.com>
2022-11-05 14:22:35 +0100

    muh shell script

--- monitoring.sh
+++ monitoring.sh
@@ -1,26 +1,45 @@
 echo -n "#Architecture: "
 uname -a
+
 echo -n "#Physical CPUs: "
 nproc --all
-echo -n "#VCPU"
+
+echo -n "#vCPUs: "
 cat /proc/cpuinfo | grep processor | wc -l
+
 echo -n "#Memory Usage: "
-free --mega | grep 'Mem:' | awk '{t = $2; f = $4; print (f, "MB free,", t, "MB total (", f/t*100, "% )")}'
+free --mega | grep 'Mem:' | awk '{printf ("%d MB free, %d MB total (%.2f%%)", $2, $4, $2/$4*100)}'
+
 echo -n "#Disk Usage (/): "
-df -h | grep '/dev/mapper/cschuijt42--vg-root' | awk '{print ($4, "used of", $2, "(", $5, ")");}' 
+df -h | grep 'cschuijt42--vg-root' | awk '{printf ("%s used of %s (%.2f%%)", $3, $2, $5)}' 
 echo -n "#Disk Usage (/home): "
-df -h | grep '/dev/mapper/cschuijt42--vg-root' | awk '{print ($4, "used of", $2, "(", $5, ")");}' 
+df -h | grep 'cschuijt42--vg-home' | awk '{printf ("%s used of %s (%.2f%%)", $3, $2, $5)}' 
+
 echo -n "#CPU Load: "
-# command
+mpstat | grep 'all' | awk '{printf("%.2f%%\n", 100-$13)}'
+
 echo -n "#Last boot: "
-# command
+who -b | awk '{print ($3, "at", $4)}'
+
 echo -n "#LVM Use: "
-# command
-echo -n "#TCP Connections: "
-# command
+if [[cat /etc/fstab | grep 'root' == /dev/mapper* ]]
+then
+echo "yes"
+else
+echo "no"
+fi
+
+echo -n "#TCP Connections established: "
+ss -s | grep 'TCP:' | awk '{printf ("%d\n", $4)}'
+
 echo -n "#Users logged in: "
-# command
+who | wc -l
+
 echo -n "#Network IP: "
-# command
+hostname -I
+
+echo -n "#Network MAC: "
+ip addr | grep 'link/ether' | awk '{print ($2)}'
+
 echo -n "#Sudo commands: "
-# command
+grep 'sudo' /var/log/auth.log | grep 'COMMAND' | wc -l
